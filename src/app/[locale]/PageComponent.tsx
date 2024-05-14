358741a80e48808bf78daab57df0be7dace67b6a
阿佐 <bgazuo@gmail.com>
2024-05-14 18:11:21 +0800

    v1

--- src/app/[locale]/PageComponent.tsx
+++ src/app/[locale]/PageComponent.tsx
@@ -1,57 +1,59 @@
-'use client'
-import {useRouter} from "next/navigation";
-import Header from '~/components/Header';
-import Footer from '~/components/Footer';
-import {useState} from "react";
-import {randomVideo} from "~/data/openaiVideo";
+"use client";
+import { useRouter } from "next/navigation";
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+import { useState } from "react";
+import { randomVideo } from "~/data/openaiVideo";
 import HeadInfo from "~/components/HeadInfo";
-import {useCommonContext} from "~/context/common-context";
+import { useCommonContext } from "~/context/common-context";
 import Link from "next/link";
 
 const PageComponent = ({
-                         locale = '',
-                         indexLanguageText,
-                         initVideoList = [],
-                         questionText
-                       }) => {
+  locale = "",
+  indexLanguageText,
+  initVideoList = [],
+  questionText,
+}) => {
   const router = useRouter();
 
-  const [textStr, setTextStr] = useState('');
-  const {setShowGeneratingModal, setShowLoadingModal} = useCommonContext();
-
+  const [textStr, setTextStr] = useState("");
+  const { setShowGeneratingModal, setShowLoadingModal } = useCommonContext();
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    setChooseAPI('FakeSora');
+    setChooseAPI("FakeSora");
     if (!textStr) {
       setVideoList(randomVideo(2));
       return;
     }
     setShowGeneratingModal(true);
     const body = {
-      prompt: textStr
+      prompt: textStr,
     };
     const response = await fetch(`/${locale}/api/generate`, {
-      method: 'POST',
-      body: JSON.stringify(body)
-    })
+      method: "POST",
+      body: JSON.stringify(body),
+    });
     const result = await response.json();
     setShowGeneratingModal(false);
     if (result.data) {
       if (!result.data[0].revised_prompt) {
-        return
+        return;
       }
       const video = {
         revised_prompt: result.data[0].revised_prompt,
-        url: result.data[0].url
-      }
+        url: result.data[0].url,
+      };
 
       // add storage
-      const videoHistoryListStr = localStorage.getItem('videoHistoryList');
+      const videoHistoryListStr = localStorage.getItem("videoHistoryList");
       if (!videoHistoryListStr) {
         const videoHistoryList = [];
         videoHistoryList.unshift(video);
-        localStorage.setItem('videoHistoryList', JSON.stringify(videoHistoryList));
+        localStorage.setItem(
+          "videoHistoryList",
+          JSON.stringify(videoHistoryList)
+        );
       } else {
         const videoHistoryList = JSON.parse(videoHistoryListStr);
         // check exist
@@ -60,21 +62,24 @@ const PageComponent = ({
           const videoHistory = videoHistoryList[i];
           if (videoHistory.revised_prompt == video.revised_prompt) {
             exist = true;
-            localStorage.setItem('video', JSON.stringify(video));
-            router.push(`/${locale}/playground`)
+            localStorage.setItem("video", JSON.stringify(video));
+            router.push(`/${locale}/playground`);
             return;
           }
         }
         if (!exist) {
           videoHistoryList.unshift(video);
           // const newList = videoHistoryList.slice(0, 3);
-          localStorage.setItem('videoHistoryList', JSON.stringify(videoHistoryList));
+          localStorage.setItem(
+            "videoHistoryList",
+            JSON.stringify(videoHistoryList)
+          );
         }
       }
-      localStorage.setItem('video', JSON.stringify(video));
-      router.push(`/${locale}/playground`)
+      localStorage.setItem("video", JSON.stringify(video));
+      router.push(`/${locale}/playground`);
     }
-  }
+  };
 
   const [videoList, setVideoList] = useState(initVideoList);
 
@@ -86,7 +91,7 @@ const PageComponent = ({
     event.target.pause();
   };
 
-  const [chooseAPI, setChooseAPI] = useState('FakeSora');
+  const [chooseAPI, setChooseAPI] = useState("FakeSora");
 
   return (
     <>
@@ -96,30 +101,43 @@ const PageComponent = ({
         locale={locale}
         page={""}
       />
-      <Header locale={locale} indexLanguageText={indexLanguageText}/>
+      <Header locale={locale} indexLanguageText={indexLanguageText} />
       <div>
-        <div className="block overflow-hidden bg-[#020d24] bg-cover bg-center text-white"
-             style={{backgroundImage: 'https://assets.website-files.com/6502af467b2a8c4ee8159a5b/6502af467b2a8c4ee8159a77_Group%2047929.svg'}}>
+        <div
+          className="block overflow-hidden bg-[#020d24] bg-cover bg-center text-white"
+          style={{
+            backgroundImage:
+              "https://assets.website-files.com/6502af467b2a8c4ee8159a5b/6502af467b2a8c4ee8159a77_Group%2047929.svg",
+          }}
+        >
           <div className="mx-auto w-full max-w-7xl px-5 mb-5">
-            <div
-              className="mx-auto flex max-w-4xl flex-col items-center text-center py-10">
-              <h1 className="mb-4 text-4xl font-bold md:text-6xl">{indexLanguageText.h1Text}</h1>
-              <div className="mb-5 max-w-[528px] lg:mb-8">
-                <p className="text-[#7c8aaa] text-xl">{indexLanguageText.pDescription}</p>
+            <div className="mx-auto flex max-w-6xl flex-col items-center text-center py-10">
+              <p className="p-4 text-blue-200 text-xl">
+                {indexLanguageText.beforeh1text}
+              </p>
+              <h1 className="mb-4 text-4xl font-bold md:text-6xl">
+                {indexLanguageText.h1Text}
+              </h1>
+              <div className="mb-5 max-w-6xl lg:mb-8">
+                <p className="text-[#7c8aaa] text-xl">
+                  {indexLanguageText.pDescription}
+                </p>
               </div>
-              <a
+              {/* <a
                 href="https://www.producthunt.com/posts/sorawebui?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sorawebui"
                 target="_blank"><img
                 src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=440945&theme=light"
                 alt="SoraWebui - Open&#0045;source&#0032;Sora&#0032;Web&#0032;Client | Product Hunt"
-              /></a>
+              /></a> */}
             </div>
-            <div>
+            {/* <div>
               <div
-                className={"w-[90%] mx-auto rounded-tl-[30px] rounded-tr-[30px] border-[12px] border-[#ffffff1f] object-fill"}>
+                className={
+                  "w-[90%] mx-auto rounded-tl-[30px] rounded-tr-[30px] border-[12px] border-[#ffffff1f] object-fill"
+                }
+              >
                 <form onSubmit={handleSubmit} className="relative shadow-lg">
-                  <div
-                    className="overflow-hidden focus-within:ring-1 focus-within:ring-indigo-500 rounded-tl-[20px] rounded-tr-[20px]">
+                  <div className="overflow-hidden focus-within:ring-1 focus-within:ring-indigo-500 rounded-tl-[20px] rounded-tr-[20px]">
                     <textarea
                       rows={8}
                       name="description"
@@ -135,34 +153,37 @@ const PageComponent = ({
                   </div>
                   <div className="flex justify-center items-center space-x-3 px-2 py-2 bg-white text-black">
                     <span
-                      className={`cursor-pointer p-2 rounded-lg mx-0.5 flex items-center mt-1 border ${chooseAPI == 'FakeSora' ? 'border-[#ffa11b]' : 'border-gray-200'}`}
-                      onClick={() => setChooseAPI('FakeSora')}
+                      className={`cursor-pointer p-2 rounded-lg mx-0.5 flex items-center mt-1 border ${
+                        chooseAPI == "FakeSora"
+                          ? "border-[#ffa11b]"
+                          : "border-gray-200"
+                      }`}
+                      onClick={() => setChooseAPI("FakeSora")}
                     >
                       <span>FakeSora</span>
                     </span>
                     <span
-                      className={`cursor-pointer p-2 rounded-lg mx-0.5 flex items-center mt-1 border ${chooseAPI == 'Sora' ? 'border-[#ffa11b]' : 'border-gray-200'}`}
-                      onClick={() => setChooseAPI('Sora')}
+                      className={`cursor-pointer p-2 rounded-lg mx-0.5 flex items-center mt-1 border ${
+                        chooseAPI == "Sora"
+                          ? "border-[#ffa11b]"
+                          : "border-gray-200"
+                      }`}
+                      onClick={() => setChooseAPI("Sora")}
                     >
                       <span>Sora</span>
                     </span>
                   </div>
-                  {
-                    chooseAPI == 'FakeSora' ?
-                      <div className="flex justify-center items-center space-x-3 px-2 bg-white text-red-400">
-                        {indexLanguageText.fakeSoraTip}
-                      </div>
-                      :
-                      chooseAPI == 'Sora' ?
-                        <div className="flex justify-center items-center space-x-3 px-2 bg-white text-red-400">
-                          {indexLanguageText.soraTip}
-                        </div>
-                        :
-                        null
-                  }
+                  {chooseAPI == "FakeSora" ? (
+                    <div className="flex justify-center items-center space-x-3 px-2 bg-white text-red-400">
+                      {indexLanguageText.fakeSoraTip}
+                    </div>
+                  ) : chooseAPI == "Sora" ? (
+                    <div className="flex justify-center items-center space-x-3 px-2 bg-white text-red-400">
+                      {indexLanguageText.soraTip}
+                    </div>
+                  ) : null}
                   <div className="inset-x-px bottom-1 bg-white">
-                    <div
-                      className="flex justify-center items-center space-x-3 border-t border-gray-200 px-2 py-2">
+                    <div className="flex justify-center items-center space-x-3 border-t border-gray-200 px-2 py-2">
                       <div className="pt-2 w-1/4">
                         <button
                           type="submit"
@@ -175,19 +196,31 @@ const PageComponent = ({
                   </div>
                 </form>
               </div>
-            </div>
+            </div> */}
 
-            <div className={"border-[14px] border-[#ffffff1f] object-fill w-[90%] mx-auto mt-8"}>
-              <div className={"mx-auto bg-white"}>
+            {/* <div
+              className={
+                "border-[14px] border-[#ffffff1f] object-fill w-[90%] mx-auto mt-8"
+              }
+            >
+              <div
+                className={
+                  "mx-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+                }
+              >
                 <div className={"pb-2 border-b-2"}>
                   <h2
-                    className={"text-blue-500 pt-4 text-4xl flex justify-center items-center"}>{indexLanguageText.soraVideoExample}</h2>
+                    className={
+                      "text-blue-500 pt-4 text-4xl flex justify-center items-center"
+                    }
+                  >
+                    {indexLanguageText.soraVideoExample}
+                  </h2>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-6 py-4">
                   {videoList.map((file) => (
                     <div key={file.prompt}>
-                      <div
-                        className="rounded-xl flex justify-center items-start">
+                      <div className="rounded-xl flex justify-center items-start">
                         <video
                           src={file.videoUrl}
                           controls={true}
@@ -197,83 +230,115 @@ const PageComponent = ({
                           controlsList={"nodownload"}
                           onMouseOver={handleMouseEnter}
                           onMouseLeave={handleMouseLeave}
-                          style={{width: '90%', height: '270px'}}
+                          style={{ width: "90%", height: "270px" }}
                         />
                       </div>
                       <div className={"flex justify-center items-center"}>
-                        <p
-                          className="pointer-events-none mt-2 block text-sm font-medium text-gray-500 w-[90%]">{indexLanguageText.prompt}: {file.prompt}</p>
+                        <p className="pointer-events-none mt-2 block text-sm font-medium text-gray-500 w-[90%]">
+                          {indexLanguageText.prompt}: {file.prompt}
+                        </p>
                       </div>
                     </div>
                   ))}
                 </div>
                 <div key={"more"} className={"px-6 py-4"}>
-                  <Link href={`/${locale}/videos`}
-                        className={"flex justify-center items-center text-xl text-red-400 hover:text-blue-600"}>
-                    {indexLanguageText.moreExample} {'>>'}
+                  <Link
+                    href={`/${locale}/videos`}
+                    className={
+                      "flex justify-center items-center text-xl text-red-400 hover:text-blue-600"
+                    }
+                  >
+                    {indexLanguageText.moreExample} {">>"}
                   </Link>
                 </div>
               </div>
-            </div>
+            </div> */}
 
-            <div className={"border-[14px] border-[#ffffff1f] object-fill w-[90%] mx-auto mt-8"}>
-              <div className={"mx-auto bg-white py-8"}>
+            <div
+              className={
+                "border-[14px] border-[#ffffff1f] object-fill w-[90%] mx-auto mt-8"
+              }
+            >
+              <div
+                className={
+                  "mx-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-8"
+                }
+              >
                 <div className={"pb-2 border-b-2"}>
                   <h2
-                    className={"text-blue-500 pt-4 text-4xl flex justify-center items-center"}>{questionText.h2_1}</h2>
+                    className={
+                      "text-white pt-4 text-4xl flex justify-center items-center"
+                    }
+                  >
+                    {questionText.h2_1}
+                  </h2>
                 </div>
-                <div className={"w-[96%] text-gray-700 prose mx-auto mt-4"}>
+                <div className={"w-[96%] text-gray-200 prose mx-auto mt-4"}>
+                  <p>{questionText.h2_1_p1}</p>
+                  <p>{questionText.h2_1_p2}</p>
+                  <p>{questionText.h2_1_p3}</p>
                   <p>
-                    {questionText.h2_1_p1}
-                  </p>
-                  <p>
-                    {questionText.h2_1_p2}
-                  </p>
-                  <p>
-                    {questionText.h2_1_p3}
-                  </p>
-                  <p>{questionText.h2_1_p4}<Link
-                    href={"https://openai.com/sora"} className={"text-blue-500"}>https://openai.com/sora</Link>.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className={"border-[14px] border-[#ffffff1f] object-fill w-[90%] mx-auto mt-8"}>
-              <div className={"mx-auto bg-white py-8"}>
-                <div className={"pb-2 border-b-2"}>
-                  <h2
-                    className={"text-blue-500 pt-4 text-4xl flex justify-center items-center"}>{questionText.h2_2}</h2>
-                </div>
-                <div className={"w-[96%] text-gray-700 prose mx-auto mt-4"}>
-                  <p>
-                    {questionText.h2_2_p1}
-                  </p>
-                  <p>
-                    {questionText.h2_2_p2}
-                  </p>
-                  <p>
-                    {questionText.h2_2_p3}
-                  </p>
-                  <p>
-                    {questionText.h2_2_p4a}<Link href={"https://sorawebui.com/"}
-                                                 className={"text-blue-500"}>https://sorawebui.com/</Link>{questionText.h2_2_p4b}<Link
-                    href={"https://github.com/SoraWebui/SoraWebui"}
-                    className={"text-blue-500"}>https://github.com/SoraWebui/SoraWebui</Link>.
+                    {questionText.h2_1_p4}
+                    <Link
+                      href={"https://openai.com/sora"}
+                      className={"text-blue-500"}
+                    >
+                      https://openai.com/sora
+                    </Link>
+                    .
                   </p>
                 </div>
               </div>
             </div>
 
+            <div
+              className={
+                "border-[14px] border-[#ffffff1f] object-fill w-[90%] mx-auto mt-8"
+              }
+            >
+              <div
+                className={
+                  "mx-auto  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-8 py-8"
+                }
+              >
+                <div className={"pb-2 border-b-2"}>
+                  <h2
+                    className={
+                      "text-white pt-4 text-4xl flex justify-center items-center"
+                    }
+                  >
+                    {questionText.h2_2}
+                  </h2>
+                </div>
+                <div className={"w-[96%] text-gray-200 prose mx-auto mt-4"}>
+                  <p>{questionText.h2_2_p1}</p>
+                  <p>{questionText.h2_2_p2}</p>
+                  <p>{questionText.h2_2_p3}</p>
+                  <p>
+                    {questionText.h2_2_p4a}
+                    <Link
+                      href={"https://sorawebui.com/"}
+                      className={"text-blue-500"}
+                    >
+                      https://sorawebui.com/
+                    </Link>
+                    {questionText.h2_2_p4b}
+                    <Link
+                      href={"https://github.com/SoraWebui/SoraWebui"}
+                      className={"text-blue-500"}
+                    >
+                      https://github.com/SoraWebui/SoraWebui
+                    </Link>
+                    .
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <Footer
-        locale={locale}
-        description={indexLanguageText.description}
-      />
+      <Footer locale={locale} description={indexLanguageText.description} />
     </>
-  )
-
-
-}
-export default PageComponent
+  );
+};
+export default PageComponent;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 const navigation = {
-  product: [
-    {name: 'SoraWebui', href: 'https://sorawebui.com'}
-  ],
+  product: [{ name: "GPT-4o.wiki", href: "https://GPT-4o.wiki.com" }],
   legal: [
-    {name: 'Privacy Policy', href: '/privacy-policy'},
-    {name: 'Terms & Conditions', href: '/terms-of-service'},
-  ]
-}
+    { name: "Privacy Policy", href: "/privacy-policy" },
+    { name: "Terms & Conditions", href: "/terms-of-service" },
+  ],
+};
 
-export default function Footer({
-                                 locale = '',
-                                 description = ''
-                               }) {
+export default function Footer({ locale = "", description = "" }) {
   return (
     <footer className="bg-[#020d24]" aria-labelledby="footer-heading">
       <div id="footer-heading" className="sr-only">
@@ -29,64 +24,64 @@ export default function Footer({
                 src="/appicon.svg"
                 width={32}
                 height={32}
-                alt="Sorawebui.com"
+                alt="GPT-4o.wiki.com"
               />
             </a>
-            <p className="text-sm text-gray-300">
-              {description}
-            </p>
+            <p className="text-sm text-gray-300">{description}</p>
           </div>
           <div className="mt-2 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
                 <div className="text-sm font-semibold leading-6 text-white"></div>
-                <ul role="list" className="mt-6 space-y-4">
-
-                </ul>
+                <ul role="list" className="mt-6 space-y-4"></ul>
               </div>
               <div className="mt-10 md:mt-0">
                 <div className="text-sm font-semibold leading-6 text-white"></div>
-                <ul role="list" className="mt-6 space-y-4">
-
-                </ul>
+                <ul role="list" className="mt-6 space-y-4"></ul>
               </div>
             </div>
             <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <div className="text-sm font-semibold leading-6 text-white">Product</div>
+              {/* <div>
+                <div className="text-sm font-semibold leading-6 text-white">
+                  Product
+                </div>
                 <ul role="list" className="mt-6 space-y-4">
                   {navigation.product.map((item) => {
-                      return (
-                        <li key={item.name}>
-                          <Link href={`${item.href}`}
-                                target={"_blank"}
-                                className="text-sm leading-6 text-gray-300 hover:text-[#2d6ae0]">
-                            {item.name}
-                          </Link>
-                        </li>
-                      )
-                    }
-                  )}
+                    return (
+                      <li key={item.name}>
+                        <Link
+                          href={`${item.href}`}
+                          target={"_blank"}
+                          className="text-sm leading-6 text-gray-300 hover:text-[#2d6ae0]"
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    );
+                  })}
                 </ul>
-              </div>
+              </div> */}
               <div className="mt-10 md:mt-0">
-                <div className="text-sm font-semibold leading-6 text-white">Legal</div>
+                <div className="text-sm font-semibold leading-6 text-white">
+                  Legal
+                </div>
                 <ul role="list" className="mt-6 space-y-4">
                   {navigation.legal.map((item) => {
-                      let hrefTo = `/${locale}${item.href}`;
-                      if (locale == 'en') {
-                        hrefTo = `${item.href}`;
-                      }
-                      return (
-                        <li key={item.name}>
-                          <Link href={`${hrefTo}`}
-                                className="text-sm leading-6 text-gray-300 hover:text-[#2d6ae0]">
-                            {item.name}
-                          </Link>
-                        </li>
-                      )
+                    let hrefTo = `/${locale}${item.href}`;
+                    if (locale == "en") {
+                      hrefTo = `${item.href}`;
                     }
-                  )}
+                    return (
+                      <li key={item.name}>
+                        <Link
+                          href={`${hrefTo}`}
+                          className="text-sm leading-6 text-gray-300 hover:text-[#2d6ae0]"
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
             </div>
@@ -94,5 +89,5 @@ export default function Footer({
         </div>
       </div>
     </footer>
-  )
+  );
 }
